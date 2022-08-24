8481edab01baae74878e69a88d348a5b5a8b0d8a
ddienu <rockdann@hotmail.com>
2022-08-23 23:10:34 -0500

    Update: tabs page
    Update tabs page extracting the bottom navigation bar widget and the pageview widget to make easier to read the Scaffold.
    Implementing two navigation bar items and block the physics in the PageView.

--- lib/pages/tabs_page.dart
+++ lib/pages/tabs_page.dart
@@ -7,23 +7,54 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        children: [
-          Container(
-            child: Center(
-              child: Text('Tab 1'),
-            ),
-            color: Colors.red,
-          ),
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
+      
+    );
+  }
+}
 
-          Container(
-            child: Center(
-              child: Text('Tab 2'),
-            ),
-            color: Colors.green,
+class _Navegacion extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline_outlined),
+          label: "Para ti",
           ),
-        ],
-      ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.public),
+          label: "Encabezados",
+          ),          
+      ]
+      );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        Container(
+          child: Center(
+            child: Text('Tab 1'),
+          ),
+          color: Colors.red,
+        ),
+
+        Container(
+          child: Center(
+            child: Text('Tab 2'),
+          ),
+          color: Colors.green,
+        ),
+      ],
     );
   }
 }
